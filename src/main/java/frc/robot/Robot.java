7b2299d9e595ecbad051e7ce5c4c084cede826d6
stone_li <sli8113@users.noreply.github.com>
2019-10-22 19:44:11 -0400

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,56 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+import edu.wpi.first.wpilibj.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.*;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+/*
+ * Simplest program to drive a robot with mecanum drive using a single Logitech
+ * Extreme 3D Pro joystick and 4 drive motors connected as follows:
+ *     - PWM 0 - Connected to front left drive motor
+ *     - PWM 1 - Connected to rear left drive motor
+ *     - PWM 2 - Connected to front right drive motor
+ *     - PWM 3 - Connected to rear right drive motor
+ */
+
+public class Robot extends TimedRobot {
+     //Create a robot drive object using PWMs 0, 1, 2 and 3
+      TalonSRX frontLeftMotor = new TalonSRX(3);	
+		  TalonSRX rearLeftMotor = new TalonSRX(0);
+		  TalonSRX frontRightMotor = new TalonSRX(1);	
+      TalonSRX rearRightMotor = new TalonSRX(2);
+      
+
+     //Define joystick being used at USB port 1 on the Driver Station
+      Joystick joystick = new Joystick(0);
+      double forward;
+      double right;
+      double clockwise;
+      
+
+
+     public void teleopPeriodic() 
+		  {
+        forward = -joystick.getY();
+        right = joystick.getX();
+        clockwise = joystick.getZ();
+
+        //front_left = forward + clockwise + right;
+        //front_right = forward - clockwise - right;
+        //rear_left = forward + clockwise - right;
+        //rear_right = forward - clockwise + right; 
+        
+        //left motors need to be reversed
+        frontLeftMotor.set(ControlMode.PercentOutput, forward + clockwise + right);
+        frontRightMotor.set(ControlMode.PercentOutput, forward - clockwise - right);
+        rearLeftMotor.set(ControlMode.PercentOutput, -(forward + clockwise - right));
+        rearRightMotor.set(ControlMode.PercentOutput, -(forward - clockwise + right));
+      }
+}
